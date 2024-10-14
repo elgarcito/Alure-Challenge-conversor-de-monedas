4b17864f564dff4e950f385e7fc5088e0c01f66f
elgaricito <edgarjaguirre@hotmail.com>
2024-10-14 20:13:58 -0300

    Corrigiendo errores

--- src/com/alura/challengeConversorDeMonedas/Main.java
+++ src/com/alura/challengeConversorDeMonedas/Main.java
@@ -9,74 +9,62 @@ public class Main {
     public static void main(String[] args) {
 
         //Valores predeterminados
-        int eleccionUsuario=0;
-        double cantidadACambiar=0;
-        ConversionMoneda conversion= new ConversionMoneda();
+        int eleccionUsuario=0;//Por defecto los valores de eleccion son nulos
+        double cantidadACambiar = 0;//Por defecto los valores de eleccion son nulos
+        Menu menu = new Menu();// Inicializo la clase menu
+        ConversionMoneda conversion = new ConversionMoneda();
         String apiKeyValue = "c096090b482a281a51695afe";
-        int estado=1;
-        Menu menu =new Menu();
-
+        int estado = 1;
         //Inicio del scanner
         Scanner lectura = new Scanner(System.in);
 
+        //Inicio del programa
 
+        //Mensaje de bienvenida
         System.out.println("Bienvenido al conversor de monedas");
-        System.out.println("Por favor elija una opcion valida"+"\n");
-        menu.mostrarOpciones();
+        System.out.println("Por favor elija una opcion valida" + "\n");
 
-        eleccionUsuario=menu.elegirOpciones(lectura);
-        if (eleccionUsuario==7){
-            estado=2;
-        }else {
-            cantidadACambiar=menu.elegirMontoACambiar(lectura);
-        }
-//        while (estado==1) {
-//            try {
-//                while (true) {
-//                    System.out.println("\n"+"Ingrese un numero del 1 al 7");
-//                    if (lectura.hasNextInt()){
-//                        eleccionUsuario = Integer.valueOf(lectura.nextLine());
-//                        if (eleccionUsuario > 0 && eleccionUsuario<=7) {
-//                            if (eleccionUsuario==7){
-//                                estado=2;
-//                            }
-//                            break;
-//                        } else {
-//                            System.out.println("Número fuera de rango. Inténtalo de nuevo.");
-//                        }}else {
-//                        System.out.println("Entrada no válida. Por favor, introduce un número.");
-//                        lectura.next();
-//                    }
-//                }
+        //Envuelvo en un while hasta que se cambie el estado
+        while (estado == 1) {
+            //Mostrar el menu con las opciones
+            menu.mostrarOpciones();
 
-//                while (estado==1) {
-//                    System.out.println("Cantidad a cambiar");
-//                    cantidadACambiar = Integer.valueOf(lectura.nextLine());
-//                    if (cantidadACambiar > 0) {
-//                        break;
-//                    }
-//                }
+            //Elegir una opcion del menu
+            eleccionUsuario = menu.elegirOpciones(lectura);
 
-                if (estado==1){
+            //Si la eleccion del menu es salir (opcion 7) el estado cambia a 2 y se termina el programa
+            if (eleccionUsuario == 7) {
+                estado = 2;
+                break;
+            } else {
+                //De otra forma accedo a la eleccion de moneda
+                cantidadACambiar = menu.elegirMontoACambiar(lectura);
                 conversion.definirConversionyMonto(eleccionUsuario, cantidadACambiar);
                 conversion.realizarConversion(apiKeyValue);
                 System.out.println(conversion);
-
-                System.out.println("\n");
-                System.out.println("Desea realizar otra conversion? 1-Si, 2-No");
-               // estado = Integer.valueOf(lectura.nextLine());
-                }
-
-                if (estado==2){
-                    System.out.println("\n");
-                    System.out.println("Muchas gracias por usar nuestro servicio :) ");
-                }else {
-                    menu.mostrarOpciones();
-                }
-
+                estado=menu.continuarOCerrarPrograma(lectura);
 
             }
+
+//            if (estado == 1) {
+//
+//
+//                System.out.println("\n");
+//                System.out.println("Desea realizar otra conversion? 1-Si, 2-No");
+//                estado = Integer.valueOf(lectura.nextLine());
+//            }
+            //Le pregunto al usuario si desea continuar
+            if (estado == 2) {
+//                System.out.println("\n");
+//                System.out.println("Muchas gracias por usar nuestro servicio :) ");
+                break;
+            } else {
+                menu.mostrarOpciones();
+            }
         }
+        System.out.println("Muchas gracias por usar nuestro servicio :) ");
+    }
+}
   //  }
 //}
 
